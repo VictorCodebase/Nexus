c643a5821ae5e8c3d37fe3805376a09ba8eaef26
victorcodebase <victorkithinji@outlook.com>
2025-02-28 09:13:01 +0300

    removed unused testing imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
 import Hero from "../components/Hero";
 import Categories from "../components/Categories";
 import Research from "../components/Research";
 
 const Home = () => {
-	const [serverRes, setServerRes] = useState({});
-
-	useEffect(() => {
-		fetch("/api?message=Hello server")
-			.then((response) => response.json())
-			.then((data) => {
-				setServerRes(data);
-				console.log(serverRes);
-			});
-	}, []);
 	return (
 		<div>
 			<Hero />
