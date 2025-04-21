6004c17ea7cabc90d062af256fd7560c9c354254
GEO-HACK <geoffreykirumba0@gmail.com>
2025-04-21 13:00:44 +0300

    fetching papers from the server

--- src/components/PaperList.jsx
+++ src/components/PaperList.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { getPapers } from "../services/paperServices";
 
 const PaperList = ({ filteredPapers }) => {
   const navigate = useNavigate();
+  // Sample papers data (Replace with API data)
+  const [papers, setPapers] = React.useState([]);
+  useEffect(() => {
+    const fetchPapers = async () => {
+      try {
+        const response = await getPapers();
+        setPapers(response.data);
+        console.log("Fetched papers:", response.data);
+      } catch (error) {
+        console.error("Error fetching papers:", error);
+      }
+    };
+    fetchPapers();
+  }, []);
+
   return (
     <>
       <div className="mb-6">
@@ -12,19 +29,24 @@ const PaperList = ({ filteredPapers }) => {
 
       {/* List View */}
       <div>
-        {filteredPapers.map((paper, index) => (
-        <Link to={`/browser/${paper.id}`} key={paper.id}>
-          <div  className="py-4 border-b last:border-none">
-            <h2 className="text-lg font-semibold">{paper.title}</h2>
+        {papers.map((paper, index) => (
+          <div className="bg-white p-4 mb-4  shadow-md" key={index}>
+            <h2 className="text-lg font-semibold">{paper.paper_name}</h2>
             <p className="text-sm text-blue-500">
-              By {paper.author} • Published: {paper.year}
+              Published: {new Date(paper.created_at).toLocaleDateString()}
             </p>
-            <p className="text-gray-700 mt-2">{paper.description}</p>
-            <button className="mt-2 text-blue-600 hover:underline">
+            <a
+              href={`http://localhost:5000/uploads${paper.file_url.replace(
+                "..",
+                ""
+              )}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-2 text-blue-600 hover:underline inline-block"
+            >
               View Paper →
-            </button>
+            </a>
           </div>
-        </Link>
         ))}
       </div>
     </>
---
+++ src/services/paperServices.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api";
+
+
+// function to get papers from the server
+export const getPapers = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/papers`);
+        return response.data;
+    }catch (error) {
+        console.error("Error fetching papers:", error);
+        return [];
+    }
+
+
+}
